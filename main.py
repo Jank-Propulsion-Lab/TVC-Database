acb45091d541efe475679a051e66fb92bc460ae9
Jank Propulsion Lab <jankpropulsionlab@gmail.com>
2022-05-24 18:34:55 -0500

    basic app

--- main.py
+++ main.py
@@ -1,1 +1,55 @@
+from sqlite3 import Row
+import tkinter as tk
+from tkinter import ttk
 
+from numpy import pad
+
+LARGE_FONT = ("Verdand", 12)
+
+class App(tk.Tk):
+    
+    def __init__(self, *args, **kwargs):
+        
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+
+        container.pack(side="top", fill="both", expand = True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (StartPage, GraphPage):
+
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+
+        
+
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Jank Database Home")
+        label.pack(padx=10, pady=10)
+
+class GraphPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Graphs")
+        label.pack(padx=10, pady=10)
+
+app = App()
+app.title("Jank Database")
+app.geometry("1280x720")
+app.mainloop()
